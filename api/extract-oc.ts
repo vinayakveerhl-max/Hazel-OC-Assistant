90e90bad28632e71acd2e83b4b9df5c1ae8f7973
vinayakveerhl-max <vinayak.veerhl@gmail.com>
2026-08-21 16:20:34 +0530

    Update import path to extractService.js

--- api/extract-oc.ts
+++ api/extract-oc.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { processPdfExtraction } from '../src/server/extractService';
+import { processPdfExtraction } from '../src/server/extractService.js';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== 'POST') {
